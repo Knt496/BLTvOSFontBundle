01a53beb44262e77dbbd32015543519212ce3a89
Kenneth Orozco <70169127+Knt496@users.noreply.github.com>
2025-05-08 15:06:35 -0600

    Update fontsBundle.sh

--- BLTvOSFontBundle/fontsBundle.sh
+++ BLTvOSFontBundle/fontsBundle.sh
@@ -1,9 +1,9 @@
-# rm -rf ./TvOSFontBundle.bundle.zip
-# rm -rf ./TvOSFontBundle.bundle
-# rm -rf ./TvOSFontBundle
-# mkdir -p ./TvOSFontBundle
-# find ./ -name "*.ttf" -exec cp -prv '{}' "./TvOSFontBundle" ";"
-# find ./ -name "*.otf" -exec cp -prv '{}' "./TvOSFontBundle" ";"
-# cp ./info.plist ./TvOSFontBundle
-# mv ./TvOSFontBundle ./TvOSFontBundle.bundle
-# zip -vr ./TvOSFontBundle.bundle.zip ./TvOSFontBundle.bundle/ -x "*.DS_Store"
+rm -rf ./TvOSFontBundle.bundle.zip
+rm -rf ./TvOSFontBundle.bundle
+rm -rf ./TvOSFontBundle
+mkdir -p ./TvOSFontBundle
+find ./ -name "*.ttf" -exec cp -prv '{}' "./TvOSFontBundle" ";"
+find ./ -name "*.otf" -exec cp -prv '{}' "./TvOSFontBundle" ";"
+cp ./info.plist ./TvOSFontBundle
+mv ./TvOSFontBundle ./TvOSFontBundle.bundle
+zip -vr ./TvOSFontBundle.bundle.zip ./TvOSFontBundle.bundle/ -x "*.DS_Store"
